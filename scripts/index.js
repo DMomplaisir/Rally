55f5e41377199a8cda0f6d1b368eabc6c5dd5118
Dylan Momplaisir <dmomplaisir@icloud.com>
2016-08-17 00:43:08 -0400

    syncing togheter
    everything is finally togheter - ish?

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,15 @@
 // Sign up flow, and register
 
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDQ-pLNBp9sv9bOkbdpB7dEWaD-XD9E5sw",
-  authDomain: "rally-37ee6.firebaseapp.com",
-  databaseURL: "https://rally-37ee6.firebaseio.com",
-  storageBucket: "rally-37ee6.appspot.com",
-};
-firebase.initializeApp(config);
+$(function(){
+
+  $('#register').hide();
+  $('#sign').click(function(){
+    $('#holder').remove();
+    $('#register').show(1000);
+  });
+  $('#signupbutton').click(function(){
+    window.location.replace("news.html");
+  })
 
 
-// first need to wait for click - and change inner html of container
-function signUp() {
-
-
-}
-
-function logIn() {
-
-
-}
+});
